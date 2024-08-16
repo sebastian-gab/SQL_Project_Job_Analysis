0fd1bd7f680589a9dee23c8da3182f84f058a2b1
sebastian-gab <g.sebastian27@outlook.com>
2024-08-16 15:51:14 +0300

    Finish query 1.01

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,15 +1,23 @@
 /*
-    Qustion: What are the top 10 paying Cloud Engineer jobs?
+    Query 1: Discover the top 10 highest-paying remote jobs for Cloud Engineers. 
 */
 
 SELECT
-    job_postings_fact.job_title_short,
-    job_postings_fact.salary_year_avg,
-    job_postings_fact.salary_hour_avg,
-    job_postings_fact.job_location
-FROM job_postings_fact
+    job_postings.job_id,
+    job_postings.job_title_short,
+    job_postings.salary_year_avg,
+    companies.name AS company_name,
+    job_postings.job_location,
+    job_postings.job_schedule_type,
+    job_postings.job_posted_date::DATE
+    
+FROM 
+    job_postings_fact AS job_postings
+LEFT JOIN company_dim AS companies ON job_postings.company_id = companies.company_id
 WHERE
-    job_postings_fact.job_title_short = 'Cloud Engineer'
+    job_postings.job_title_short = 'Cloud Engineer' AND
+    job_postings.salary_year_avg IS NOT NULL AND
+    job_postings.job_location = 'Anywhere'
 ORDER BY
-    job_postings_fact.salary_year_avg DESC
+    job_postings.salary_year_avg DESC
 LIMIT 10;
